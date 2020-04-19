b03c6f633ff8dbaee0521de27c666b177a6b1059
rackar <27681135@qq.com>
2020-04-19 23:15:17 +0800

    能创建群了

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,6 +39,6 @@ module.exports = {
     'prefer-promise-reject-errors': 'off',
 
     // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn'
   }
 }
--- src/pages/group/GroupCreate.vue
+++ src/pages/group/GroupCreate.vue
@@ -12,14 +12,15 @@
         <div class="createGroupLeft left">
           <div class="groupName">
             <label class="left">小组名称</label>
-            <input class="left" v-model="crtgrp.tit" type="text" placeholder="必填" />
+            <!-- <input class="left" v-model="title" type="text" placeholder="" /> -->
+            <q-input square outlined v-model="title" label="必填" />
             <div class="clearfix"></div>
           </div>
           <div class="groupDescription">
             <label class="left">小组介绍</label>
             <textarea
               class="left"
-              v-model="crtgrp.desc_text"
+              v-model="desc_text"
               maxlength="200"
               placeholder="描述下你的话题，引起大家的兴趣"
             ></textarea>
@@ -33,7 +34,7 @@
             <label class="radioLabel">
               <input
                 class="postRadio allPerson"
-                v-model="crtgrp.read_permission"
+                v-model="read_permission"
                 type="radio"
                 value="1"
                 name="postRadio"
@@ -45,23 +46,18 @@
               <input
                 class="postRadio onlyTeam"
                 type="radio"
-                v-model="crtgrp.read_permission"
+                v-model="read_permission"
                 value="2"
                 name="postRadio"
               />
               <span class="radioInput"></span>仅限组员&nbsp;&nbsp;
             </label>
           </div>
-          <div class="settingContent" v-if="crtgrp.read_permission==2">
+          <div class="settingContent" v-if="read_permission==2">
             <label class="setingLabel">设置密码</label>
-            <input
-              class="settingPws"
-              v-model="crtgrp.password"
-              type="password"
-              placeholder="6位数字或字母"
-            />
+            <input class="settingPws" v-model="password" type="password" placeholder="6位数字或字母" />
           </div>
-          <div class="popupBtn" @click="creategrp">完成</div>
+          <div class="popupBtn" @click="createGroup">完成</div>
         </div>
         <div class="createGroupRight right">
           创建群之后你还可以设置以下参数：入群奖励、阅读奖励、回复奖励等。
@@ -83,26 +79,36 @@ export default {
   props: {},
   data() {
     return {
-      crtgrp: { tit: "", desc_text: "", read_permission: "", password: "" }
+      title: "",
+      desc_text: "",
+      read_permission: "",
+      password: ""
     };
   },
   watch: {},
   computed: {},
   methods: {
-    async creategrp() {
-      let apiCode = "/protected/grp/create";
-      let val = this.crtgrp;
-      val["name"] = val.tit;
-      val.read_permission = Number(val.read_permission);
-      if ((val.tit = "" || val.desc_text == "" || val.read_permission == "")) {
+    async createGroup() {
+      let data = {
+        name: this.title,
+        desc_text: this.desc_text,
+        read_permission: Number(this.read_permission),
+        password: this.password,
+        avatar: ""
+      };
+      if (
+        data.name == "" ||
+        data.desc_text == "" ||
+        data.read_permission == ""
+      ) {
         this.$q.notify("信息不完整，请完善信息");
       } else {
-        const putgrps = await this.$axios.post("/" + apiCode, val);
-        if (putgrps.code == 0) {
-          this.getgrp();
-          this.iscrtgrp();
+        let apiCode = "/protected/grp/create";
+        const result = await this.$axios.post(apiCode, data);
+        if (result.data.code == 0) {
+          this.$q.notify("创建成功，需手动刷新页面");
         } else {
-          this.$q.notify(putgrps.massage);
+          this.$q.notify(result.massage);
         }
       }
     }
