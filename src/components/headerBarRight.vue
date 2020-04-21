43837b15ad14c7a29dae6c7be126dfd5cf32cefc
rackar <27681135@qq.com>
2020-04-21 18:05:29 +0800

    add temp config

--- src/components/headerBarRight.vue
+++ src/components/headerBarRight.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <q-avatar class="clickable q-mx-xs" rounded size="36px" icon="notifications">
+    <q-avatar
+      class="clickable q-mx-xs"
+      rounded
+      size="36px"
+      icon="notifications"
+      @click="showNotice=!showNotice"
+    >
       <q-badge color="red" floating>4</q-badge>
     </q-avatar>
     <q-avatar
@@ -11,6 +17,17 @@
     >
       <img :src="$store.state.user.avatar||'statics/user.svg'" />
     </q-avatar>
+    <q-dialog v-model="showNotice">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">show Notice</div>
+          <div
+            v-for="notice in notifications"
+            :key="notice.id"
+          >{{notice.h_text}} - {{$utils.timeStringToLocal(create_at)}}</div>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
 
@@ -19,7 +36,18 @@ export default {
   components: {},
   props: {},
   data() {
-    return {};
+    return {
+      showNotice: false,
+      notifications: [
+        {
+          create_at: "2020-04-11T20:36:04+08:00",
+          h_text: "恭喜你，10个ETH",
+          id: 1,
+          user: 1,
+          read: 1
+        }
+      ]
+    };
   },
   watch: {},
   computed: {},
---
+++ src/pages/person/ReDetails.vue
@@ -0,0 +1,135 @@
+<template>
+  <div>
+    个人信息部分
+    <div class="row q-gutter-lg">
+      <div class="col-6 offset-md-1">
+        <q-card flat class="my-card">
+          <q-tabs
+            v-model="tab"
+            dense
+            class="text-grey"
+            active-color="primary"
+            indicator-color="primary"
+            align="justify"
+            narrow-indicator
+          >
+            <q-tab name="myPosts" label="我的帖子" />
+            <q-tab name="recycle" label="回收站" />
+          </q-tabs>
+
+          <q-separator />
+
+          <q-tab-panels v-model="tab" animated>
+            <q-tab-panel name="myPosts">
+              <ArticleShow
+                v-for="post in pullList"
+                :post="post"
+                :key="post.post.id"
+                :personPage="true"
+              />
+            </q-tab-panel>
+
+            <q-tab-panel name="recycle">
+              <div class="text-h6">回收站</div>
+            </q-tab-panel>
+          </q-tab-panels>
+        </q-card>
+      </div>
+      <div class="col-4">
+        <q-card>我的钱包</q-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ArticleShow from "pages/article/ArticleShow";
+export default {
+  components: { ArticleShow },
+  props: {},
+  data() {
+    return {
+      tab: "myPosts",
+      userinfo: {},
+      mycode: [],
+      tokens: [],
+      log: [],
+
+      pullList: []
+    };
+  },
+  watch: {},
+  computed: {},
+  methods: {
+    // 个人信息
+    getuserinfo: async function() {
+      let url = "/protected/user/me";
+      const resData = await this.$axios.get(url, {});
+      if (resData.data.code == 0) {
+        this.userinfo = resData.data.data.me;
+        this.$emit("setUserinfo", this.userinfo);
+      } else if (resData.code == 104) {
+        this.$router.push({ path: "/login", query: {} });
+      }
+    },
+    // 我的代币
+    getmycode: async function() {
+      let url = "protected/user/tokens";
+      const resCode = await this.$axios.get(url, {});
+      if (resCode.data.code == 0) {
+        this.tokens = resCode.data.data.tokens;
+      } else if (resCode.data.code == 104) {
+      }
+    },
+    // 我的帖子
+    getmyabb: async function(del) {
+      let url = "protected/post/my/pull";
+      const resCode = await this.$axios.post(url, {
+        base_post: null,
+        deleted: del
+      });
+      if (resCode.data.code == 0) {
+        this.pullList = [];
+        this.pullList = resCode.data.data.posts;
+        // if (!this.pullList.length) {
+        // }
+      }
+    }, // 转账日志
+    tokenLog: async function() {
+      let url = "protected/user/token/logs";
+      let dat = {
+        contract: "",
+        base_token_log: null
+      };
+      const resLog = await this.$axios.post(url, dat);
+      if (resLog.data.code == 0) {
+        this.log = resLog.data.data.token_logs;
+      } else {
+        alert(resLog.data.message);
+      }
+    }
+  },
+  created() {
+    let userid = this.$store.state.user.userid;
+    if (userid) {
+      //注释掉接口初始化
+      this.getuserinfo();
+      this.getmycode();
+      this.tokenLog();
+      this.getmyabb(false);
+      //   this.$nextTick(function() {
+      //     this.doImg();
+      //     this.hideinfo();
+      //   });
+    } else {
+      this.$router.push({ path: "/login", query: {} });
+    }
+  },
+  mounted() {}
+};
+</script>
+<style lang="scss" scoped>
+.my-card {
+  max-width: 400px;
+}
+</style>
