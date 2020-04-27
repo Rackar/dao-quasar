1e65a585573d17b4e72b45f8c215dc0d02e2cf75
rackar <27681135@qq.com>
2020-04-27 23:11:56 +0800

    清除残留代码，去掉aws-sdk库

--- src/pages/group/GroupList.vue
+++ src/pages/group/GroupList.vue
@@ -118,7 +118,7 @@ export default {
     this.init();
   },
   watch: {
-    $route(to, from) {
+    $route(to) {
       if (to.fullPath === '/') {
         this.init();
       }
--- src/pages/person/wallet.vue
+++ src/pages/person/wallet.vue
@@ -3,22 +3,27 @@
     <q-card-section class="row no-wrap">
       我的钱包
       <q-space />
-      转 二
+      <q-btn flat>
+        <q-icon name="img:statics/icons/icon_code_normal.svg" />
+      </q-btn>
+      <q-btn flat>
+        <q-icon name="img:statics/icons/icon_zhuanzhang_normal.svg" />
+      </q-btn>
     </q-card-section>
     <q-card-section class="text-center">
-      <q-btn flat label="总金额 (NES)▽">
+      <q-btn flat :label="'总金额 ('+currentToken.name+')▽'">
         <q-menu auto-close>
           <q-list style="min-width: 100px">
-            <q-item clickable>
-              <q-item-section>Ethereum ETH</q-item-section>
-            </q-item>
-            <q-separator />
-            <q-item clickable>
-              <q-item-section>DAO NES</q-item-section>
-            </q-item>
+            <div v-for="token in tokens" :key="token.id">
+              <q-item clickable @click="pickToken(token)">
+                <q-item-section>{{token.contract.name}}</q-item-section>
+              </q-item>
+              <q-separator />
+            </div>
           </q-list>
         </q-menu>
       </q-btn>
+      <div>{{currentToken.value}}</div>
     </q-card-section>
     <q-card-section>
       <q-list style="min-width: 100px">
@@ -40,13 +45,38 @@ export default {
     log: Array,
   },
   data() {
-    return {};
+    return {
+      currentToken: {
+        name: '',
+        contract: '',
+        value: '0.00',
+      },
+    };
   },
-  watch: {},
+  watch: {
+    tokens(newToken) {
+      if (newToken && newToken.length) {
+        this.pickToken(newToken[0]);
+      }
+    },
+  },
   computed: {},
-  methods: {},
-  created() {},
+  methods: {
+    pickToken(token) {
+      this.currentToken = {
+        name: token.contract.name,
+        symbol: token.contract.symbol,
+        contract: token.contract.contract,
+        value: token.token.value,
+      };
+    },
+  },
+  created() {
+    if (this.tokens && this.tokens.length) {
+      this.pickToken(this.tokens[0]);
+    }
+  },
   mounted() {},
 };
 </script>
-<style lang="scss"></style>
+
