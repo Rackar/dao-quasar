2c6d2dba7cf3684123e799999e9af97b40220916
rackar <27681135@qq.com>
2020-05-05 23:38:35 +0800

    全局时间格式化工具完善

--- src/boot/utils.js
+++ src/boot/utils.js
@@ -11,10 +11,22 @@ function timeStringToLocal(timestring) {
   }
 
   let t = new Date(timestring);
+  let formNow = Math.floor((new Date() - t) / 1000);
+  let string;
+  if (formNow < 60) {
+    string = `${formNow}秒前`;
+  } else if (formNow < 3600) {
+    string = `${Math.floor(formNow / 60)}分钟前`;
+  } else if (formNow < 3600 * 24) {
+    string = `${Math.floor(formNow / 3600)}小时前`;
+  } else if (formNow < 3600 * 24 * 4) {
+    string = `${Math.floor(formNow / 3600 / 24)}天前`;
+  } else {
+    string = `${t.getFullYear()}-${addZero(t.getMonth() + 1)}-${addZero(t.getDate())} ${addZero(
+      t.getHours()
+    )}:${addZero(t.getMinutes())}`;
+  }
 
-  let string = `${t.getFullYear()}-${addZero(t.getMonth() + 1)}-${addZero(t.getDate())} ${addZero(
-    t.getHours()
-  )}:${addZero(t.getMinutes())}`;
   return string;
 }
 
